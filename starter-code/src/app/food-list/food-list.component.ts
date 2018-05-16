fd3d1a1ad4ef3ef773c0954fd55ee11f89c7ce0d
Richard Batista <stalfos@icloud.com>
2018-05-16 09:46:35 -0400

    Iteration 1

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -2,15 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+	selector: 'app-food-list',
+	templateUrl: './food-list.component.html',
+	styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+	foodList: Array<any> = [];
+	newFood: any = {name: "", calories: "", image: ""};
+	isFormShowing: boolean = false;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	addNewFood() {
+		const theNewFood = {
+			name: this.newFood.name,
+			calories: this.newFood.calories,
+			image: this.newFood.image
+		}
+		this.foodList.unshift(theNewFood);
+		this.newFood = {name: "", calories: "", image: ""};
+		this.isFormShowing = false;
+	}
 
+	toggleForm() {
+		this.isFormShowing = !this.isFormShowing;
+	}
+
+	ngOnInit() {
+		this.foodList = foods;
+	}
 }
